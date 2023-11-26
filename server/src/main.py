875900e78595c6f8fc4013d9db94b427216608bc
EddieTheCubeHead <eetu.asikainen1204@gmail.com>
2023-11-26 13:15:41 +0200

    Uvicorn and FastAPI logging uniform with our own logger

--- server/src/main.py
+++ server/src/main.py
@@ -1,13 +1,19 @@
 from logging import getLogger
 
+from uvicorn import run
+
 from application import application
 from logging_conf import setup_logging
 
-app = application
 setup_logging()
+app = application
 _logger = getLogger("main.main")
 
 
 @app.get("/")
 async def root():
     _logger.debug("Root called")
+
+
+if __name__ == '__main__':
+    run(app, log_config=None)
