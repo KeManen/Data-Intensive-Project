6f704cac76da55618323e4cb359a7aaaf15375d7
EddieTheCubeHead <eetu.asikainen1204@gmail.com>
2023-12-10 15:41:11 +0200

    Docker compose base for server

--- server/src/main.py
+++ server/src/main.py
@@ -1,6 +1,8 @@
+import os
 from logging import getLogger
 
 from uvicorn import run
+
 from logging_conf import setup_logging
 from routes import app
 
@@ -9,4 +11,4 @@ if __name__ == '__main__':
     setup_logging()
     _logger = getLogger("main.main")
     _logger.info("Running uvicorn")
-    run(app, log_config=None)
+    run(app, log_config=None, host=os.getenv("host", default="127.0.0.1"))
