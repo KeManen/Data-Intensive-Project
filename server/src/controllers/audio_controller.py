74758026a3c20952e0187ef8bb87d175379caaf9
KeMa <keskimattinen.konsta@gmail.com>
2023-12-15 20:06:23 +0200

    feat:created main controllers and linked routes

---
+++ server/src/controllers/audio_controller.py
@@ -0,0 +1,21 @@
+
+
+async def get_audio_data(audio_data_id):
+    yield NotImplementedError
+
+async def post_audio_data():
+    yield NotImplementedError
+
+
+async def delete_audio_data(audio_data_id):
+    yield NotImplementedError
+
+async def get_audio_info(audio_data_id):
+    yield NotImplementedError
+
+async def post_audio_info():
+    yield NotImplementedError
+
+
+async def delete_audio_info(audio_data_id):
+    yield NotImplementedError
