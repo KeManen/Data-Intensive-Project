00d18d5816620fd1c6315aff746bfcb49ec2a29a
Otto Åhlfors <75956314+OttoAhlfors@users.noreply.github.com>
2023-12-17 13:58:13 +0200

    Docker (#15)
    * Next.JS setup done
    
    * Main page incomplete
    
    * Removed douples
    
    * Added rest controller
    
    * Added dockerfile
    
    * feat: included frontend and regions dockerfile
    
    * fixed docker
    
    ---------
    
    Co-authored-by: KeManen <dev@kema.fi>

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,14 +1,74 @@
 'use client'
-import { useUser } from '@/app/ui/UserProvider';
+
+import {Box, Button, Card, Grid, InputBase, Typography, alpha, styled } from "@mui/material";
+import Masonry from '@mui/lab/Masonry';
+import SearchIcon from '@mui/icons-material/Search';
+import { useEffect, useState } from "react";
+import { get, post } from './api/restController';
+
+const Search = styled('div')(({ theme }) => ({
+  width: 300,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  }
+}));
+
+const musicLibraryList = ["Rock", "Pop", "Hip-hop", "Rap", "Jazz", "Blues", "Country", "Electronic", "Classical", "Reggae", "Metal", "Indie"]
 
 export default function Home() {
+  const [musicLibraryList, setMusicLibraryList] = useState<string[]>([])
 
-  const  { isLoggedIn, user } = useUser();
+  const getMusicLibrarys = () => {
+    get<{ yourData: string }>('/audio_collection/1')
+      .then(response => {
+        console.log('GET Response:', response.data);
+      })
+      .catch(error => {
+        console.error('GET Error:', error);
+      });
+  }
 
+
+  useEffect(() => {
+    const onWindowLoad = () => {
+      //const response =  getMusicLibrarys()
+      getMusicLibrarys()
+      //setMusicLibraryList(response) 
+    }
+    onWindowLoad()
+  }, [])
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>This is dashboard</div>
+    <main className="p-2 pt-20">
+      <Masonry columns={2} spacing={2}>
+        <Card sx={{height: 200}}>
+          <Search>
+            <SearchIcon/>
+            <InputBase
+              sx={{paddingLeft: 1}}
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+        </Card>
+        <Card sx={{height: 700}}>
+          <div className="p-2">
+            <Grid container spacing={2}>
+              {musicLibraryList.map((musicLibraryName, index) => (
+                <Grid item xs={4} key={index}> 
+                <Card sx={{height: 100, width: 100}}>
+                  {musicLibraryName}
+                </Card>
+              </Grid>
+              ))}
+            </Grid>
+          </div>
+        </Card>
+        <Card sx={{height: 484}}>
+          <Typography >Placeholder</Typography>
+        </Card>
+      </Masonry>
     </main>
   )
 }
