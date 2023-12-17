450b4a903dc18c3b0b1aa0385cfd74ec89e9a71a
EddieTheCubeHead <eetu.asikainen1204@gmail.com>
2023-12-17 11:08:50 +0200

    Slightly better tests

--- server/test/domain/auth_features.py
+++ server/test/domain/auth_features.py
@@ -4,4 +4,5 @@ from src.domain.authentication import salt_and_hash, validate
 def should_salt_and_hash_a_password():
     password = "My unique password"
     salted = salt_and_hash(password)
+    assert salted != password
     assert validate(password, salted)
