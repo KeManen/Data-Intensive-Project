f70361e7b4321f58f56cd1de317a54c79b04ef36
Eetu Asikainen <eetu.asikainen1204@gmail.com>
2023-12-17 17:14:27 +0200

    Init global and song database, song fetching and adding infrastructure (#21)
    * Change mongo image for docker
    
    * Save and fetch songs from mongo
    
    * Song infrastructure. Data init for global db

--- server/src/database/sql/global_connection.py
+++ server/src/database/sql/global_connection.py
@@ -23,6 +23,19 @@ def test_db():
         _logger.debug("Test over")
 
 
+def init_regions():
+    with connection_manager.session() as session:
+        euro = get_currency_from_name("Euro") or Currency(name="Euro", code="EUR", rate_to_usd=1.08)
+        if get_region_from_name("eu") is None:
+            session.add(Region(name="eu", currency=euro))
+
+        dollar = get_currency_from_name("Dollar") or Currency(name="Dollar", code="USD", rate_to_usd=1)
+        if get_region_from_name("us") is None:
+            session.add(Region(name="us", currency=dollar))
+
+        session.commit()
+
+
 def create_user_login(user_name: str, password_hash: str, region_id: int) -> UserLogin:
     with connection_manager.session() as session:
         session.add(UserLogin(user_name=user_name, password_hash_salt=password_hash, region_id=region_id))
@@ -33,3 +46,23 @@ def create_user_login(user_name: str, password_hash: str, region_id: int) -> Use
 def get_user_login(user_name: str) -> UserLogin:
     with connection_manager.session() as session:
         return session.scalar(select(UserLogin).where(UserLogin.name == user_name))
+
+
+def get_region(region_id: int) -> Region:
+    with connection_manager.session() as session:
+        return session.scalar(select(Region).where(Region.id == region_id))
+
+
+def get_region_from_name(region_name: str) -> Region:
+    with connection_manager.session() as session:
+        return session.scalar(select(Region).where(Region.name == region_name))
+
+
+def get_currency(currency_id: int) -> Currency:
+    with connection_manager.session() as session:
+        return session.scalar(select(Currency).where(Currency.id == currency_id))
+
+
+def get_currency_from_name(currency_name: str) -> Currency:
+    with connection_manager.session() as session:
+        return session.scalar(select(Currency).where(Currency.name == currency_name))
